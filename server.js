d1be94ccf45cd83e588ddcc96531967cf823b752
Archana Nagaraja <johndoe@example.com>
2021-03-28 09:40:06 -0700

    added post code

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const path = require('path');
 // helper function
 const helpers = require('./utils/helpers');
 
-// // handlebars
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({helpers});
+// handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
 
 // session (connects session to sequelize Database)
 const session = require('express-session');
@@ -16,27 +16,27 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// // create session 
-// const sess = {
-//   secret: "super super secret",
-//   cookie: { originalMaxAge: 600000 },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+// create session 
+const sess = {
+  secret: "super super secret",
+  cookie: { originalMaxAge: 600000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// // set Handlebars as the default template engine
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+// set Handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
